2f3a64959bc99c8fe8756646bcfeb412f26b9241
Eric Eastwood <contact@ericeastwood.com>
2025-02-08 16:41:06 -0600

    Fix pointer to stack memory problems in `ChildProcessRunner`
     - https://github.com/MadLittleMods/zig-x-compositing-manager/blob/7aae4875e2fe96e393d7f5da1f9264527785ac8e/src/main.zig#L916-L938
     - https://github.com/MadLittleMods/zig-x-compositing-manager/blob/7aae4875e2fe96e393d7f5da1f9264527785ac8e/src/main.zig#L931-L933

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,8 @@ const builtin = @import("builtin");
 const assertions = @import("utils/assertions.zig");
 const assert = assertions.assert;
 const string_utils = @import("utils/string_utils.zig");
+const child_process_utils = @import("utils/child_process_utils.zig");
+const ChildProcessRunner = child_process_utils.ChildProcessRunner;
 const x = @import("x");
 const common = @import("x11/x11_common.zig");
 const render = @import("compositing_manager/render.zig");
@@ -753,240 +755,6 @@ pub fn main() !void {
     }
 }
 
-const ChildOutput = struct {
-    child_process: *const std.ChildProcess,
-    stdout: std.ArrayList(u8),
-    stderr: std.ArrayList(u8),
-    allocator: std.mem.Allocator,
-
-    /// The process must be started with `stdout_behavior` and `stderr_behavior` set to `.Pipe`
-    /// ```
-    /// child_process.stdout_behavior = .Pipe;
-    /// child_process.stderr_behavior = .Pipe;
-    /// ```
-    fn init(child_process: *const std.ChildProcess, allocator: std.mem.Allocator) ChildOutput {
-        assert(
-            child_process.stdout_behavior == .Pipe,
-            "child_process.stdout_behavior must be set to .Pipe but saw {}",
-            .{child_process.stdout_behavior},
-        );
-        assert(
-            child_process.stderr_behavior == .Pipe,
-            "child_process.stderr_behavior must be set to .Pipe {}",
-            .{child_process.stderr_behavior},
-        );
-
-        return .{
-            .child_process = child_process,
-            .stdout = std.ArrayList(u8).init(allocator),
-            .stderr = std.ArrayList(u8).init(allocator),
-            .allocator = allocator,
-        };
-    }
-
-    fn deinit(self: @This()) void {
-        self.stdout.deinit();
-        self.stderr.deinit();
-    }
-
-    /// Collect the output text from a child process (stdout and stderr).
-    fn collectOutputFromChildProcess(self: *@This()) void {
-        self.child_process.collectOutput(
-            &self.stdout,
-            &self.stderr,
-            std.math.maxInt(usize),
-        ) catch |err| {
-            // Add a placeholder in `stderr` that we failed to collect the logs. This is
-            // the most obvious way I can think to communicate this to the user but
-            // feels a bit indirect.
-            const error_message = std.fmt.allocPrint(
-                self.allocator,
-                "<Failed to collect output from child process: {}>\n",
-                .{err},
-            ) catch |allocation_err| {
-                // We will give up and just log if we're down to allocation errors
-                std.log.err("ChildOutput: Failed to format error message: {} after encountering {}\n", .{ allocation_err, err });
-                return;
-            };
-            defer self.allocator.free(error_message);
-
-            self.stderr.appendSlice(error_message) catch |allocation_err| {
-                // We will give up and just log if we're down to allocation errors
-                std.log.err("ChildOutput: Failed to append to stderr: {} after encountering {}\n", .{ allocation_err, err });
-            };
-        };
-    }
-};
-
-/// Print some debug logs of what the child process printed out (stdout and stderr).
-fn printChildOutput(child_output: ChildOutput, allocator: std.mem.Allocator) !void {
-    const stdout_separator_spacer = try string_utils.repeatString(
-        "=",
-        try string_utils.findLengthOfPrintedValue(child_output.stdout.items.len, "{d}", allocator),
-        allocator,
-    );
-    defer allocator.free(stdout_separator_spacer);
-    std.debug.print("================= stdout start ({d}) =================\n{s}\n================= stdout end {s}======================\n\n", .{
-        child_output.stdout.items.len,
-        child_output.stdout.items,
-        stdout_separator_spacer,
-    });
-
-    const stderr_separator_spacer = try string_utils.repeatString(
-        "=",
-        try string_utils.findLengthOfPrintedValue(child_output.stderr.items.len, "{d}", allocator),
-        allocator,
-    );
-    defer allocator.free(stderr_separator_spacer);
-    std.debug.print("================= stderr start ({d}) =================\n{s}\n================= stderr end {s}======================\n\n", .{
-        child_output.stderr.items.len,
-        child_output.stderr.items,
-        stderr_separator_spacer,
-    });
-}
-
-pub const Term = union(enum) {
-    Running: void,
-    Exited: u8,
-    Signal: u32,
-    Stopped: u32,
-    Unknown: u32,
-    NotFound: void,
-};
-
-/// Check if a process is still running, or not.
-///
-/// Modified version of `std.os.waitpid` so we can handle "still running" and "not
-/// found" cases (more friendly to work with).
-pub fn waitpid(pid: std.os.pid_t, flags: u32) Term {
-    const Status = if (builtin.link_libc) c_int else u32;
-    var status: Status = undefined;
-    const coerced_flags = if (builtin.link_libc) @as(c_int, @intCast(flags)) else flags;
-    while (true) {
-        const rc = std.os.system.waitpid(pid, &status, coerced_flags);
-        switch (std.os.errno(rc)) {
-            // If `pid` is 0, the process is still running (no state change yet)
-            .SUCCESS => {
-                const wait_result = std.os.WaitPidResult{
-                    .pid = @as(std.os.pid_t, @intCast(rc)),
-                    .status = @as(u32, @bitCast(status)),
-                };
-
-                // If `pid` is 0, the process is still running (no state change yet)
-                if (wait_result.pid == 0) {
-                    return Term.Running;
-                } else {
-                    return statusToTerm(wait_result.status);
-                }
-            },
-            .INTR => continue,
-            // The process specified does not exist.
-            .CHILD => return Term.NotFound,
-            .INVAL => unreachable, // Invalid flags.
-            else => unreachable,
-        }
-    }
-}
-
-// via`std.ChildProcess.statusToTerm`
-fn statusToTerm(status: u32) Term {
-    return if (std.os.W.IFEXITED(status))
-        .{ .Exited = std.os.W.EXITSTATUS(status) }
-    else if (std.os.W.IFSIGNALED(status))
-        .{ .Signal = std.os.W.TERMSIG(status) }
-    else if (std.os.W.IFSTOPPED(status))
-        .{ .Stopped = std.os.W.STOPSIG(status) }
-    else
-        .{ .Unknown = status };
-}
-
-/// Wrapper around ChildProcess that can handle collecting logs and some utilities for
-/// working with X11 applications.
-const ChildProcessRunner = struct {
-    description: []const u8,
-    child_output: ChildOutput,
-    child_process: *std.ChildProcess,
-    allocator: std.mem.Allocator,
-
-    fn init(
-        description: []const u8,
-        argv: []const []const u8,
-        allocator: std.mem.Allocator,
-    ) !@This() {
-        var child_process = std.ChildProcess.init(argv, allocator);
-        // The default behavior is `.Inherit` which will muddy up test runner `stdout`
-        // and make the test runner hang, see
-        // https://github.com/ziglang/zig/issues/15091
-        //
-        // We set `stdout` and `stderr` to `.Pipe` so we can see the output if the build
-        // fails.
-        child_process.stdin_behavior = .Ignore;
-        child_process.stdout_behavior = .Pipe;
-        child_process.stderr_behavior = .Pipe;
-
-        // Start the test_window process.
-        try child_process.spawn();
-
-        // Start collecting logs
-        var child_output = ChildOutput.init(&child_process, allocator);
-        // TODO: asdf uncomment
-        // _ = try std.Thread.spawn(.{}, ChildOutput.collectOutputFromChildProcess, .{&child_output});
-
-        return .{
-            .description = description,
-            .child_output = child_output,
-            .child_process = &child_process,
-            .allocator = allocator,
-        };
-    }
-
-    fn deinit(self: @This()) void {
-        std.debug.print("asdf deinit\n", .{});
-        self.child_output.deinit();
-
-        // Kill the process when we're done
-        _ = self.child_process.kill() catch |err| {
-            std.debug.print("Failed to kill {s}: {}\n", .{ self.description, err });
-        };
-    }
-
-    fn waitForProcessToExitSuccessfully(self: *@This()) !void {
-        std.debug.print("asdf {}, {any}\n", .{ self.child_process.id, self.child_process.term });
-        const term = try self.child_process.wait();
-        std.testing.expectEqual(std.ChildProcess.Term{ .Exited = 0 }, term) catch |err| {
-            // Give some more context on the failure
-            std.debug.print("{s} failed to exit and finish successfully\n", .{self.description});
-            try printChildOutput(self.child_output, self.allocator);
-
-            // Return the original assertion error
-            return err;
-        };
-    }
-
-    /// Wait for the X Window to be ready.
-    ///
-    /// This is useful so we get consistent stacking orders of the test windows or make
-    /// sure the compositing manager is ready to composite things before we spawn more
-    /// windows.
-    ///
-    /// This isn't a perfect solution as we should ideally wait for the window to be
-    /// mapped but it should be good enough.
-    fn waitForProcessWindowToBeReady(
-        self: @This(),
-        x_window_finder: *XWindowFinder,
-        timeout_ms: u64,
-    ) !void {
-        x_window_finder.waitForProcessWindowToBeReady(self.child_process.id, timeout_ms) catch |err| {
-            // Give some more context what happened
-            std.debug.print("Failed to find the window for {s}\n", .{self.description});
-            try printChildOutput(self.child_output, self.allocator);
-
-            // Return the original assertion error
-            return err;
-        };
-    }
-};
-
 test {
     // https://ziglang.org/documentation/master/#Nested-Container-Tests
     @import("std").testing.refAllDecls(@This());
@@ -1025,7 +793,7 @@ test "end-to-end" {
     // Start the compositing manager process.
     const main_process_runner = try ChildProcessRunner.init(
         "main compositing manager",
-        &[_][]const u8{ "zig", "build", "main" },
+        &[_][]const u8{"./zig-out/bin/main"},
         allocator,
     );
     defer main_process_runner.deinit();
@@ -1090,13 +858,13 @@ test "end-to-end" {
         test_window_process_runner3,
     };
     for (running_processes) |running_process| {
-        const term = waitpid(
+        const term = child_process_utils.waitpid(
             running_process.child_process.id,
             // We specify `NOHANG` so we don't block and wait for a signal change. We
             // just want to check the state as it is now.
             std.os.W.NOHANG,
         );
-        if (term != Term.Running) {
+        if (term != child_process_utils.Term.Running) {
             std.debug.print(
                 "Expected the {s} process to be still be successfully running " ++
                     "at the end of the test but saw {any}\n",
@@ -1104,7 +872,7 @@ test "end-to-end" {
             );
 
             // Give some more context on process might have exited
-            try printChildOutput(running_process.child_output, allocator);
+            try child_process_utils.printChildOutput(running_process.child_output, allocator);
 
             return error.ProcessUnexpectedlyNoLongerRunning;
         }
---
+++ src/utils/child_process_utils.zig
@@ -0,0 +1,246 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const assertions = @import("assertions.zig");
+const assert = assertions.assert;
+const string_utils = @import("string_utils.zig");
+const XWindowFinder = @import("../x11/x_window_finder.zig").XWindowFinder;
+
+pub const ChildOutput = struct {
+    child_process: *const std.ChildProcess,
+    stdout: std.ArrayList(u8),
+    stderr: std.ArrayList(u8),
+    allocator: std.mem.Allocator,
+
+    /// The process must be started with `stdout_behavior` and `stderr_behavior` set to `.Pipe`
+    /// ```
+    /// child_process.stdout_behavior = .Pipe;
+    /// child_process.stderr_behavior = .Pipe;
+    /// ```
+    fn init(child_process: *const std.ChildProcess, allocator: std.mem.Allocator) ChildOutput {
+        assert(
+            child_process.stdout_behavior == .Pipe,
+            "child_process.stdout_behavior must be set to .Pipe but saw {}",
+            .{child_process.stdout_behavior},
+        );
+        assert(
+            child_process.stderr_behavior == .Pipe,
+            "child_process.stderr_behavior must be set to .Pipe {}",
+            .{child_process.stderr_behavior},
+        );
+
+        return .{
+            .child_process = child_process,
+            .stdout = std.ArrayList(u8).init(allocator),
+            .stderr = std.ArrayList(u8).init(allocator),
+            .allocator = allocator,
+        };
+    }
+
+    fn deinit(self: @This()) void {
+        self.stdout.deinit();
+        self.stderr.deinit();
+    }
+
+    /// Collect the output text from a child process (stdout and stderr).
+    ///
+    /// `stdout` and `stderr` are only filled once once all output has been collected
+    /// (in most cases, that's when the process exits).
+    fn collectOutputFromChildProcess(self: *@This()) void {
+        self.child_process.collectOutput(
+            &self.stdout,
+            &self.stderr,
+            std.math.maxInt(usize),
+        ) catch |err| {
+            // Add a placeholder in `stderr` that we failed to collect the logs. This is
+            // the most obvious way I can think to communicate this to the user but
+            // feels a bit indirect.
+            const error_message = std.fmt.allocPrint(
+                self.allocator,
+                "<Failed to collect output from child process: {}>\n",
+                .{err},
+            ) catch |allocation_err| {
+                // We will give up and just log if we're down to allocation errors
+                std.log.err("ChildOutput: Failed to format error message: {} after encountering {}\n", .{ allocation_err, err });
+                return;
+            };
+            defer self.allocator.free(error_message);
+
+            self.stderr.appendSlice(error_message) catch |allocation_err| {
+                // We will give up and just log if we're down to allocation errors
+                std.log.err("ChildOutput: Failed to append to stderr: {} after encountering {}\n", .{ allocation_err, err });
+            };
+        };
+    }
+};
+
+/// Print some debug logs of what the child process printed out (stdout and stderr).
+pub fn printChildOutput(child_output: *const ChildOutput, allocator: std.mem.Allocator) !void {
+    const stdout_separator_spacer = try string_utils.repeatString(
+        "=",
+        try string_utils.findLengthOfPrintedValue(child_output.stdout.items.len, "{d}", allocator),
+        allocator,
+    );
+    defer allocator.free(stdout_separator_spacer);
+    std.debug.print("================= stdout start ({d}) =================\n{s}\n================= stdout end {s}======================\n\n", .{
+        child_output.stdout.items.len,
+        child_output.stdout.items,
+        stdout_separator_spacer,
+    });
+
+    const stderr_separator_spacer = try string_utils.repeatString(
+        "=",
+        try string_utils.findLengthOfPrintedValue(child_output.stderr.items.len, "{d}", allocator),
+        allocator,
+    );
+    defer allocator.free(stderr_separator_spacer);
+    std.debug.print("================= stderr start ({d}) =================\n{s}\n================= stderr end {s}======================\n\n", .{
+        child_output.stderr.items.len,
+        child_output.stderr.items,
+        stderr_separator_spacer,
+    });
+}
+
+pub const Term = union(enum) {
+    Running: void,
+    Exited: u8,
+    Signal: u32,
+    Stopped: u32,
+    Unknown: u32,
+    NotFound: void,
+};
+
+/// Check if a process is still running, or not.
+///
+/// Modified version of `std.os.waitpid` so we can handle "still running" and "not
+/// found" cases (more friendly to work with).
+pub fn waitpid(pid: std.os.pid_t, flags: u32) Term {
+    const Status = if (builtin.link_libc) c_int else u32;
+    var status: Status = undefined;
+    const coerced_flags = if (builtin.link_libc) @as(c_int, @intCast(flags)) else flags;
+    while (true) {
+        const rc = std.os.system.waitpid(pid, &status, coerced_flags);
+        switch (std.os.errno(rc)) {
+            // If `pid` is 0, the process is still running (no state change yet)
+            .SUCCESS => {
+                const wait_result = std.os.WaitPidResult{
+                    .pid = @as(std.os.pid_t, @intCast(rc)),
+                    .status = @as(u32, @bitCast(status)),
+                };
+
+                // If `pid` is 0, the process is still running (no state change yet)
+                if (wait_result.pid == 0) {
+                    return Term.Running;
+                } else {
+                    return statusToTerm(wait_result.status);
+                }
+            },
+            .INTR => continue,
+            // The process specified does not exist.
+            .CHILD => return Term.NotFound,
+            .INVAL => unreachable, // Invalid flags.
+            else => unreachable,
+        }
+    }
+}
+
+// via`std.ChildProcess.statusToTerm`
+fn statusToTerm(status: u32) Term {
+    return if (std.os.W.IFEXITED(status))
+        .{ .Exited = std.os.W.EXITSTATUS(status) }
+    else if (std.os.W.IFSIGNALED(status))
+        .{ .Signal = std.os.W.TERMSIG(status) }
+    else if (std.os.W.IFSTOPPED(status))
+        .{ .Stopped = std.os.W.STOPSIG(status) }
+    else
+        .{ .Unknown = status };
+}
+
+/// Wrapper around ChildProcess that can handle collecting logs and some utilities for
+/// working with X11 applications.
+pub const ChildProcessRunner = struct {
+    description: []const u8,
+    child_output: *ChildOutput,
+    child_process: *std.ChildProcess,
+    allocator: std.mem.Allocator,
+
+    pub fn init(
+        description: []const u8,
+        argv: []const []const u8,
+        allocator: std.mem.Allocator,
+    ) !@This() {
+        var child_process = try allocator.create(std.ChildProcess);
+        child_process.* = std.ChildProcess.init(argv, allocator);
+        // The default behavior is `.Inherit` which will muddy up test runner `stdout`
+        // and make the test runner hang, see
+        // https://github.com/ziglang/zig/issues/15091
+        //
+        // We set `stdout` and `stderr` to `.Pipe` so we can see the output if the build
+        // fails.
+        child_process.stdin_behavior = .Ignore;
+        child_process.stdout_behavior = .Pipe;
+        child_process.stderr_behavior = .Pipe;
+
+        // Start the test_window process.
+        try child_process.spawn();
+
+        // Start collecting logs
+        var child_output = try allocator.create(ChildOutput);
+        child_output.* = ChildOutput.init(child_process, allocator);
+        // This thread will be killed by TODO
+        _ = try std.Thread.spawn(.{}, ChildOutput.collectOutputFromChildProcess, .{child_output});
+
+        return .{
+            .description = description,
+            .child_output = child_output,
+            .child_process = child_process,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: @This()) void {
+        self.child_output.deinit();
+        self.allocator.destroy(self.child_output);
+
+        // Kill the process when we're done
+        _ = self.child_process.kill() catch |err| {
+            std.debug.print("Failed to kill {s}: {}\n", .{ self.description, err });
+        };
+
+        self.allocator.destroy(self.child_process);
+    }
+
+    pub fn waitForProcessToExitSuccessfully(self: *@This()) !void {
+        const term = try self.child_process.wait();
+        std.testing.expectEqual(std.ChildProcess.Term{ .Exited = 0 }, term) catch |err| {
+            // Give some more context on the failure
+            std.debug.print("{s} failed to exit and finish successfully\n", .{self.description});
+            try printChildOutput(self.child_output, self.allocator);
+
+            // Return the original assertion error
+            return err;
+        };
+    }
+
+    /// Wait for the X Window to be ready.
+    ///
+    /// This is useful so we get consistent stacking orders of the test windows or make
+    /// sure the compositing manager is ready to composite things before we spawn more
+    /// windows.
+    ///
+    /// This isn't a perfect solution as we should ideally wait for the window to be
+    /// mapped but it should be good enough.
+    pub fn waitForProcessWindowToBeReady(
+        self: @This(),
+        x_window_finder: *XWindowFinder,
+        timeout_ms: u64,
+    ) !void {
+        x_window_finder.waitForProcessWindowToBeReady(self.child_process.id, timeout_ms) catch |err| {
+            // Give some more context what happened
+            std.debug.print("Failed to find the window for {s}\n", .{self.description});
+            try printChildOutput(self.child_output, self.allocator);
+
+            // Return the original assertion error
+            return err;
+        };
+    }
+};
